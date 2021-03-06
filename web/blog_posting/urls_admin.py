def01dba36ad20abdb1e7645e64c22c0de44f3ad
Fahri Reza <dozymoe@gmail.com>
2021-03-06 14:47:50 +0700

    manage BlogPosting pages

--- web/blog_posting/urls_admin.py
+++ web/blog_posting/urls_admin.py
@@ -6,8 +6,11 @@ app_name = 'blog_posting'
 
 urlpatterns = [
     path('add', views_admin.Create.as_view(), name='Create'),
-    path('<int:pk>/edit', views_admin.Create.as_view(), name='Update'),
-    path('<int:pk>/delete', views_admin.Create.as_view(), name='Destroy'),
+    path('<int:pk>/edit', views_admin.Edit.as_view(), name='Update'),
+    path('<int:pk>/delete', views_admin.Destroy.as_view(), name='Destroy'),
+    path('<int:pk>/publish', views_admin.Publish.as_view(), name='Publish'),
+    path('<int:pk>/unpublish', views_admin.Unpublish.as_view(),
+        name='Unpublish'),
     path('<str:slug>-<int:pk>', views_admin.Display.as_view(), name='Display'),
     path('', views_admin.List.as_view(), name='Index'),
 ]
--- web/blog_posting/views_admin.py
+++ web/blog_posting/views_admin.py
@@ -2,9 +2,11 @@ import logging
 #-
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DeleteView, DetailView, ListView
 from django.views.generic import UpdateView
+from rules.contrib.views import AutoPermissionRequiredMixin
 #-
 from .models import BlogPosting
 
@@ -38,15 +40,46 @@ class Display(DetailView):
 
 
 @method_decorator(login_required, name='dispatch')
-class Edit(UpdateView):
+class Edit(AutoPermissionRequiredMixin, UpdateView):
     model = BlogPosting
+    fields = ['title', 'body', 'summary', 'slug']
 
     def get_success_url(self):
         return reverse('BlogPosting:Index')
 
 
 @method_decorator(login_required, name='dispatch')
-class Destroy(DeleteView):
+class Publish(AutoPermissionRequiredMixin, UpdateView):
+    model = BlogPosting
+    fields = []
+    template_name_suffix = '_confirm_publish'
+
+    def form_valid(self, form):
+        form.instance.published_at = timezone.now()
+        return super().form_valid(form)
+
+
+    def get_success_url(self):
+        return reverse('BlogPosting:Index')
+
+
+@method_decorator(login_required, name='dispatch')
+class Unpublish(AutoPermissionRequiredMixin, UpdateView):
+    model = BlogPosting
+    fields = []
+    template_name_suffix = '_confirm_unpublish'
+
+    def form_valid(self, form):
+        form.instance.published_at = None
+        return super().form_valid(form)
+
+
+    def get_success_url(self):
+        return reverse('BlogPosting:Index')
+
+
+@method_decorator(login_required, name='dispatch')
+class Destroy(AutoPermissionRequiredMixin, DeleteView):
     model = BlogPosting
 
     def get_success_url(self):
