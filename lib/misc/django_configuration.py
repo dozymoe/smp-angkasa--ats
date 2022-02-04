efafd86fe6ef1d7a3f2d9900302254de9570116c
Fahri Reza <dozymoe@gmail.com>
2022-02-04 15:45:41 +0700

    typo

--- lib/misc/django_configuration.py
+++ lib/misc/django_configuration.py
@@ -4,5 +4,6 @@ from .configuration import flatten_dict
 def django_conf_adapter(prefix, new, setter):
     exceptions = [
         'database.options',
+        'logging.handlers',
     ]
     setter.update(flatten_dict(prefix, new, exceptions))
