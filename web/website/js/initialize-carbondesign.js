dbba85866c5b448e8afe73b33354591490649cbf
Gitea <gitea@fake.local>
2022-03-02 04:48:54 +0700

    table overflow menu

--- web/website/js/initialize-carbondesign.js
+++ web/website/js/initialize-carbondesign.js
@@ -1,6 +1,7 @@
 import init_checkbox from '../../misc/carbondesign-js/auto/checkbox';
 import init_data_table from '../../misc/carbondesign-js/auto/data-table';
 import init_date_picker from '../../misc/carbondesign-js/auto/date-picker';
+import init_overflow_menu from '../../misc/carbondesign-js/auto/overflow-menu';
 import init_text_input from '../../misc/carbondesign-js/auto/text-input';
 import init_ui_shell from '../../misc/carbondesign-js/auto/ui-shell';
 
@@ -9,6 +10,7 @@ export default function(app)
     init_checkbox();
     init_data_table();
     init_date_picker();
+    init_overflow_menu();
     init_text_input();
     init_ui_shell();
 }
