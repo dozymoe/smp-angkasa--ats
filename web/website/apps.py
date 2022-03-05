de52ef0d67966d938410d4160013930c5d851793
Gitea <gitea@fake.local>
2022-03-05 20:58:27 +0700

    embed youtube

--- web/website/apps.py
+++ web/website/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+from docutils.parsers.rst import directives
 
 
 class WebsiteConfig(AppConfig):
     name = 'website'
+
+    def ready(self):
+        # pylint:disable=import-outside-toplevel
+        from .restructured_text.embed_youtube import EmbedYoutubeDirective
+
+        directives.register_directive('youtube', EmbedYoutubeDirective)
---
+++ web/website/restructured_text/embed_youtube.py
@@ -0,0 +1,59 @@
+import logging
+#-
+from docutils import nodes
+from docutils.parsers.rst import Directive, directives
+
+_logger = logging.getLogger(__name__)
+
+DEFAULT_WIDTH = 320
+DEFAULT_HEIGHT = 200
+
+def parse_embed_youtube(video_id, options, block_text=None):
+    template = """
+<iframe src="https://www.youtube.com/embed/%(video_id)s" width="%(width)u"
+    height="%(height)u" frameborder="0" webkitAllowFullScreen mozallowfullscreen
+    allowfullscreen></iframe>
+"""
+    values = {
+        'video_id': video_id,
+        'width': options.get('width', DEFAULT_WIDTH),
+        'height': options.get('height', DEFAULT_HEIGHT),
+    }
+    return [nodes.raw('', template % values, format='html')]
+
+
+class EmbedYoutubeDirective(Directive):
+
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = True
+    option_spec = {
+        'alt': directives.unchanged,
+        'height': directives.length_or_unitless,
+        'width': directives.length_or_percentage_or_unitless,
+        'name': directives.unchanged,
+        'class': directives.class_option,
+    }
+
+    def run(self):
+        """ Entry point.
+
+        Check:
+          * self.name
+          * self.arguments
+          * self.options
+          * self.content
+          * self.lineno
+          * self.content_offset
+          * self.block_text
+          * self.state
+          * self.state_machine
+
+        """
+        video_id = self.arguments[0]
+
+        _nodes = parse_embed_youtube(video_id, self.options, self.block_text)
+        for node in _nodes:
+            self.add_name(node)
+
+        return _nodes
